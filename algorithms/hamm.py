d9402f53f9cd67476484a5c71435b15312b64148
protoxpire0 <fahim.shahreer95@gmail.com>
2017-09-16 02:24:21 -0400

    edited codes

--- algorithms/hamm.py
+++ algorithms/hamm.py
@@ -1,6 +1,3 @@
-from Bio import Entrez
-from Bio import SeqIO
-
 def countPointMutation (dna1, dna2):
     count = 0
     for i in range(len(dna1)):
@@ -13,25 +10,10 @@ def findFirstDiff (dna1, dna2):
         if not dna1[i] == dna2[i]:
             return (i)
     return None
-#
-# if __name__ ==  "__main__":
-#     with open("hamm.txt") as file:
-#         dna1 = file.readline().strip()
-#         dna2 = file.readline().strip()
-#         print (countPointMutation(dna1, dna2))
-#         print (findFirstDiff(dna1, dna2))
 
-
-def getDNAfromFasta (fastafile):
-    path = "./fasta/" + fastafile
-    file = open(path, 'r')
-    seq_record = SeqIO.read(file, "fasta")
-    sequence_object = seq_record.seq
-    dna = str(sequence_object)
-    return dna
-
-if __name__ == "__main__":
-    dna1 = getDNAfromFasta("test1-1.fasta")
-    dna2 = getDNAfromFasta("test1-2.fasta")
-    print (countPointMutation(dna1, dna2))
-    print (dna1[:findFirstDiff(dna1, dna2)])
+if __name__ ==  "__main__":
+    with open("hamm.txt") as file:
+        dna1 = file.readline().strip()
+        dna2 = file.readline().strip()
+        print (countPointMutation(dna1, dna2))
+        print (findFirstDiff(dna1, dna2))
--- algorithms/rna.py
+++ algorithms/rna.py
@@ -1,6 +1,3 @@
-from Bio import Entrez
-from Bio import SeqIO
-
 def transcribe (dna):
     rna = ""
     for nucleotide in dna:
@@ -10,25 +7,7 @@ def transcribe (dna):
             rna = rna + nucleotide
     return (rna)
 
-
-
-def getDNAfromFasta (fastafile):
-    path = "./fasta/" + fastafile
-    with open(path, 'r') as file:
-        seq_record = SeqIO.read(file, "fasta")
-    sequence_object = seq_record.seq
-    dna = str(sequence_object)
-    return dna
-
-if __name__ == "__main__":
-    dna1 = getDNAfromFasta("test1-1.fasta")
-    dna2 = getDNAfromFasta("test1-2.fasta")
-    print (transcribe(dna1))
-    print (transcribe(dna2))
-
-
-
-# if __name__ ==  "__main__":
-#     with open("rna.txt") as file:
-#         dna = file.readline().strip()
-#     print (transcribe(dna))
+if __name__ ==  "__main__":
+    with open("rna.txt") as file:
+        dna = file.readline().strip()
+    print (transcribe(dna))
